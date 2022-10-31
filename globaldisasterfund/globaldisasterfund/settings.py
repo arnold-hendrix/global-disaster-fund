cf1d4a1339f60976af3ef215fae10ee68774f126
Arnold Masanga <masaar01@luther.edu>
2022-10-31 10:05:13 -0400

    static files config

--- globaldisasterfund/globaldisasterfund/settings.py
+++ globaldisasterfund/globaldisasterfund/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 
